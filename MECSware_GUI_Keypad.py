34db7e967e78fbc14e72143ae270bb42b5126a4a
Bastian Kolb-Grunder <76182879+Boisti13@users.noreply.github.com>
2024-06-13 12:57:27 +0200

    update

--- MECSware_GUI_Keypad.py
+++ MECSware_GUI_Keypad.py
@@ -355,7 +355,7 @@ ip_entry.grid(row=0, column=1, columnspan=1)
 
 port_label = ttk.Label(frame, text="Port:", width=width_c, anchor=tk.E, font = standard_font)
 port_label.grid(row=0, column=2)
-port_entry = ttk.Entry(frame, width=width_c-5, justify='center', font = standard_font)
+port_entry = ttk.Entry(frame, width=width_c-10, justify='center', font = standard_font)
 port_entry.insert(0, port_initial)
 port_entry.grid(row=0, column=3, columnspan=1, padx=(10, 10))
 
